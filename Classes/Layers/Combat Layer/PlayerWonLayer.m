f4c93f5136b676328c9d45353f24faac07ded3e3
Eric Stenborg <estenborg@gmail.com>
2014-01-13 22:03:56 -0500

    Added the win and loss layers post combat.  Added persistant xp gain and leveling for player’s monster when player wins combat.

---
+++ Classes/Layers/Combat Layer/PlayerWonLayer.m
@@ -0,0 +1,14 @@
+//
+//  PlayerWonLayer.m
+//  BabyDragon
+//
+//  Created by Eric Stenborg on 1/13/14.
+//  Copyright 2014 First Light Games. All rights reserved.
+//
+
+#import "PlayerWonLayer.h"
+
+
+@implementation PlayerWonLayer
+
+@end
