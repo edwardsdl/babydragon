76b3f04f8fc67d2cdfd7625d88a5377146184772
Dallas Edwards <git-tower@dallasedwards.com>
2014-01-14 00:38:03 -0500

    Removed class header comments

--- Classes/Level/Floor.h
+++ Classes/Level/Floor.h
@@ -1,11 +1,3 @@
-//
-//  Floor.h
-//  BabyDragon
-//
-//  Created by Dallas Edwards on 1/14/14.
-//  Copyright (c) 2014 First Light Games. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 @interface Floor : NSObject
--- Classes/Level/Tile.m
+++ Classes/Level/Tile.m
@@ -1,11 +1,3 @@
-//
-//  Tile.m
-//  BabyDragon
-//
-//  Created by Dallas Edwards on 1/14/14.
-//  Copyright (c) 2014 First Light Games. All rights reserved.
-//
-
 #import "Tile.h"
 
 @implementation Tile
