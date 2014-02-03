b7bc9d897078c5dd0d93fb2ebed6e35caa04629f
Dallas Edwards <git-tower@dallasedwards.com>
2014-02-02 21:50:26 -0500

    Fleshed out SPC core data methods

--- Classes/Core Data/Management/SinglePlayerContentData+Management.h
+++ Classes/Core Data/Management/SinglePlayerContentData+Management.h
@@ -3,4 +3,14 @@
 
 @interface SinglePlayerContentData (Management)
 
++ (NSArray *)allSinglePlayerContent;
++ (SinglePlayerContentData *)insertSinglePlayerContentWithEnvironmentType:(EnvironmentType)environmentType
+                                                              lootQuality:(LootQualityType)lootQuality
+                                                      minimumMonsterLevel:(int)minimumMonsterLevel
+                                                      maximumMonsterLevel:(int)maximumMonsterLevel
+                                                                     seed:(int)seed
+                                                                  barcode:(NSString *)barcode;
++ (void)removeAll;
++ (SinglePlayerContentData *)singlePlayerContentWithSeed:(int)seed;
+
 @end
--- Classes/Core Data/Management/SinglePlayerContentData+Management.m
+++ Classes/Core Data/Management/SinglePlayerContentData+Management.m
@@ -1,5 +1,64 @@
+#import "CoreDataHelper.h"
+#import "EnvironmentType.h"
+#import "LootQualityType.h"
+#import "NSManagedObjectContext+Management.h"
 #import "SinglePlayerContentData+Management.h"
 
 @implementation SinglePlayerContentData (Management)
+
+static NSString *entityName = @"SinglePlayerContentData";
+
++ (NSArray *)allSinglePlayerContent
+{
+    NSManagedObjectContext *managedObjectContext = [[CoreDataHelper sharedInstance] managedObjectContext];
+    
+    return [managedObjectContext objectsForEntityName:entityName];
+}
+
++ (SinglePlayerContentData *)insertSinglePlayerContentWithEnvironmentType:(EnvironmentType)environmentType
+                                                              lootQuality:(LootQualityType)lootQuality
+                                                      minimumMonsterLevel:(int)minimumMonsterLevel
+                                                      maximumMonsterLevel:(int)maximumMonsterLevel
+                                                                     seed:(int)seed
+                                                                  barcode:(NSString *)barcode
+{
+    NSManagedObjectContext *managedObjectContext = [[CoreDataHelper sharedInstance] managedObjectContext];
+    
+    SinglePlayerContentData *singlePlayerContentData = [NSEntityDescription insertNewObjectForEntityForName:entityName
+                                                                                     inManagedObjectContext:managedObjectContext];
+    [singlePlayerContentData setBarcode:barcode];
+    [singlePlayerContentData setEnvironmentType:environmentType];
+    [singlePlayerContentData setLootQuality:lootQuality];
+    [singlePlayerContentData setMaximumMonsterLevel:maximumMonsterLevel];
+    [singlePlayerContentData setMinimumMonsterLevel:minimumMonsterLevel];
+    [singlePlayerContentData setSeed:seed];
+
+    return singlePlayerContentData;
+}
+
++ (void)removeAll
+{
+    NSManagedObjectContext *managedObjectContext = [[CoreDataHelper sharedInstance] managedObjectContext];
+    
+    for (SinglePlayerContentData *singlePlayerContent in [self allSinglePlayerContent])
+    {
+        [managedObjectContext deleteObject:singlePlayerContent];
+    }
+    
+    NSError *error = nil;
+    [managedObjectContext save:&error];
+}
+
++ (SinglePlayerContentData *)singlePlayerContentWithSeed:(int)seed
+{
+    NSManagedObjectContext *managedObjectContext = [[CoreDataHelper sharedInstance] managedObjectContext];
+    
+    NSArray * results = [managedObjectContext objectsFromFetchRequestTemplateWithName:@"GetSinglePlayerContentWithSeed"
+                                                                substitutionVariables:@{@"seed": [NSNumber numberWithInt:seed]}];
+    
+    return [results count] > 0
+    ? (SinglePlayerContentData *)[results objectAtIndex:0]
+    : nil;
+}
 
 @end
