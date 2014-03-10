f20178c24b52afe6504efd18a1920cfab2dbeaf9
Dallas Edwards <git-tower@dallasedwards.com>
2014-03-10 00:06:01 -0400

    Added test classes

---
+++ Tests/ItemDataTests.m
@@ -0,0 +1,26 @@
+#import <XCTest/XCTest.h>
+
+@interface ItemDataTests : XCTestCase
+
+@end
+
+@implementation ItemDataTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
