bab7673b5d3bc1de958a1aa5fb3cd79dffcb55c7
Dallas Edwards <git-tower@dallasedwards.com>
2014-04-21 00:08:07 -0400

    Added single player content tests

---
+++ Tests/SinglePlayerContentTests.m
@@ -0,0 +1,24 @@
+#import <XCTest/XCTest.h>
+
+@interface SinglePlayerContentTests : XCTestCase
+
+@end
+
+@implementation SinglePlayerContentTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+}
+
+- (void)testGeneratingIdenticalItems
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
