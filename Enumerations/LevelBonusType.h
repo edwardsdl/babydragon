96934f9aed7df604f455091a92ee9a2cb79fa95c
Dallas Edwards <git-tower@dallasedwards.com>
2014-04-21 00:08:38 -0400

    Added level bonus type enumeration

---
+++ Enumerations/LevelBonusType.h
@@ -0,0 +1,14 @@
+typedef NS_ENUM(int32_t, LevelBonusType) {
+    NoBonus,
+    SmallGoldBonus,
+    MediumGoldBonus,
+    LargeGoldBonus,
+    SmallExperienceBonus,
+    MediumExperienceBonus,
+    LargeExperienceBonus,
+    
+    // HACK: This value exists because it isn't possible to iterate over enumerations in
+    // Objective-C. Do not place any values after this. See http://stackoverflow.com/a/6910685
+    // for more information.
+    LevelBonusTypeCount
+};
