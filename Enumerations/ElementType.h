093dd25cb152ca097378ae34ed183c34b9e69440
Dallas Edwards <git-tower@dallasedwards.com>
2014-04-21 00:09:52 -0400

    Element type enumeration cleanup

--- Enumerations/ElementType.h
+++ Enumerations/ElementType.h
@@ -1,8 +1,13 @@
 typedef NS_ENUM(int32_t, ElementType) {
-    ElementTypeWater,
+    ElementTypeNone,
+    ElementTypeDarkness,
     ElementTypeEarth,
     ElementTypeFire,
-    ElementTypeDarkness,
     ElementTypeHoly,
-    ElementTypeNone
+    ElementTypeWater,
+    
+    // HACK: This value exists because it isn't possible to iterate over enumerations in
+    // Objective-C. Do not place any values after this. See http://stackoverflow.com/a/6910685
+    // for more information.
+    ElementTypeCount
 };
